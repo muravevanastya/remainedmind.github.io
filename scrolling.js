3413e2b01c11ca87b419c95b12673d475be8b03d
remainedmind <lemoonadresse@gmail.com>
2023-05-29 21:45:41 +0300

    working on

--- scrolling.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href*="#"]').forEach(anchor => anchor.addEventListener("click", function (e) {
-    const anchorTargetID = anchor.getAttribute("href").substring(1);
-    const $anchorTarget = document.getElementById(anchorTargetID);
-
-    if (!$anchorTarget) return;
-
-    e.preventDefault();
-    $anchorTarget.scrollIntoView({ behavior: "smooth", block: "start" });
-}));
