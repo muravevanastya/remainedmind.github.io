996a24be4013cf698469c990d3cd179377a2bd5a
remainedmind <lemoonadresse@gmail.com>
2023-05-29 12:30:35 +0300

    added scrolling

---
+++ scrolling.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('a[href*="#"]').forEach(anchor => anchor.addEventListener("click", function (e) {
+    const anchorTargetID = anchor.getAttribute("href").substring(1);
+    const $anchorTarget = document.getElementById(anchorTargetID);
+
+    if (!$anchorTarget) return;
+
+    e.preventDefault();
+    $anchorTarget.scrollIntoView({ behavior: "smooth", block: "start" });
+}));
